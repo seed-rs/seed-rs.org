f1dbf5e1c8aa905103e91858391e4791a635d5c2
Martin Kavík <martin@kavik.cz>
2019-10-15 01:55:15 +0200

    generated html guides

---
+++ crate/build.rs
@@ -0,0 +1,74 @@
+use pulldown_cmark::{self, Event, Tag};
+use std::fs;
+use std::path::PathBuf;
+
+fn main() {
+    for path in html_files() {
+        fs::remove_file(path).unwrap();
+    }
+
+    for path in markdown_files() {
+        let markdown = fs::read_to_string(&path).unwrap();
+        let html = markdown_to_html(&markdown);
+
+        let html_path = format!("guides/generated_html/{}.html", path.file_stem().unwrap().to_str().unwrap());
+        fs::write(html_path, html).unwrap();
+    }
+}
+
+fn html_files() -> Vec<PathBuf> {
+    fs::read_dir("guides/generated_html")
+        .unwrap()
+        .filter_map(|entry| {
+            let path = entry.unwrap().path();
+            if path.extension().unwrap_or_default() == "html" {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+fn markdown_files() -> Vec<PathBuf> {
+    fs::read_dir("guides")
+        .unwrap()
+        .filter_map(|entry| {
+            let path = entry.unwrap().path();
+            if path.extension().unwrap_or_default() == "md" {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+fn markdown_to_html(markdown: &str) -> String {
+    let parser = pulldown_cmark::Parser::new(markdown);
+    let parser_with_syntax_highlighting = highlight_syntax(parser);
+    let mut html = String::new();
+    pulldown_cmark::html::push_html(&mut html, parser_with_syntax_highlighting);
+    html
+}
+
+fn highlight_syntax<'a, I>(parser: I) -> impl Iterator<Item = Event<'a>>
+    where
+        I: Iterator<Item = Event<'a>>,
+{
+    parser
+//    parser.map(|event| {
+//        match event {
+////            Event::Start(Tag::CodeBlock(code_block)) => {
+////                Event::Start(Tag::CodeBlock("ZXC".into()))
+////            }
+//            Event::Text(text) => {
+//                Event::Text("VBN".into())
+//            }
+//            _ => event
+//        }
+//    })
+}
+
+
+
--- crate/src/guide.rs
+++ crate/src/guide.rs
@@ -17,62 +17,62 @@ pub fn guides() -> Vec<Guide> {
         Guide {
             slug: "quickstart",
             menu_title: "Quickstart",
-            content: include_str!("quickstart.md"),
+            content: include_str!(concat!("../guides/generated_html/", "quickstart.html")),
         },
         Guide {
             slug: "prereqs",
             menu_title: "Prereqs",
-            content: include_str!("prereqs.md"),
+            content: include_str!(concat!("../guides/generated_html/", "prereqs.html")),
         },
         Guide {
             slug: "structure",
             menu_title: "Structure",
-            content: include_str!("structure.md"),
+            content: include_str!(concat!("../guides/generated_html/", "structure.html")),
         },
         Guide {
             slug: "events",
             menu_title: "Events",
-            content: include_str!("events.md"),
+            content: include_str!(concat!("../guides/generated_html/", "events.html")),
         },
         Guide {
             slug: "components",
             menu_title: "Components",
-            content: include_str!("components.md"),
+            content: include_str!(concat!("../guides/generated_html/", "components.html")),
         },
         Guide {
             slug: "http-requests-and-state",
             menu_title: "Http requests and state",
-            content: include_str!("fetch.md"),
+            content: include_str!(concat!("../guides/generated_html/", "fetch.html")),
         },
         Guide {
             slug: "routing",
             menu_title: "Routing",
-            content: include_str!("routing.md"),
+            content: include_str!(concat!("../guides/generated_html/", "routing.html")),
         },
         Guide {
             slug: "misc-features",
             menu_title: "Misc features",
-            content: include_str!("misc.md"),
+            content: include_str!(concat!("../guides/generated_html/", "misc.html")),
         },
         Guide {
             slug: "release-and-debugging",
             menu_title: "Release and debugging",
-            content: include_str!("release_and_debugging.md"),
+            content: include_str!(concat!("../guides/generated_html/", "release_and_debugging.html")),
         },
         Guide {
             slug: "complex-apps",
             menu_title: "Complex apps",
-            content: include_str!("complex_apps.md"),
+            content: include_str!(concat!("../guides/generated_html/", "complex_apps.html")),
         },
         Guide {
             slug: "server-integration",
             menu_title: "Server integration",
-            content: include_str!("server_integration.md"),
+            content: include_str!(concat!("../guides/generated_html/", "server_integration.html")),
         },
         Guide {
             slug: "about",
             menu_title: "About",
-            content: include_str!("about.md"),
+            content: include_str!(concat!("../guides/generated_html/", "about.html")),
         },
     ]
 }
