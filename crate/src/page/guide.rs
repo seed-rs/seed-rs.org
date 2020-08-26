47688634a522d7efacf3edd18875ec9b33d77124
Martin Kavík <martin@kavik.cz>
2020-08-26 14:13:11 +0200

    fix: code-blocks web-component update and details reinitialization

--- crate/src/page/guide.rs
+++ crate/src/page/guide.rs
@@ -7,10 +7,6 @@ use crate::{
     Guide, Model, Msg,
 };
 use seed::{prelude::*, *};
-use std::{
-    collections::hash_map::DefaultHasher,
-    hash::{Hash, Hasher},
-};
 
 pub fn view(guide: &Guide, model: &Model, show_intro: bool) -> Node<Msg> {
     div![
@@ -109,7 +105,7 @@ fn view_guide_html(content: &str) -> Node<Msg> {
             // it has to be "markdown-body" so it's content is styled by Github CSS
             C.markdown_body,
         ],
-        set_keys_to_code_blocks(raw!(content))
+        set_el_keys(raw!(content))
     ]
 }
 
@@ -134,19 +130,18 @@ fn view_netlify_logo() -> Node<Msg> {
     ]
 }
 
-/// Add element key to each code block to force reinitialization if code changed.
-fn set_keys_to_code_blocks(nodes: Vec<Node<Msg>>) -> Vec<Node<Msg>> {
+/// Add element keys to force reinitialization on page change.
+fn set_el_keys(nodes: Vec<Node<Msg>>) -> Vec<Node<Msg>> {
     nodes
         .into_iter()
         .map(|node| match node {
-            Node::Element(el) => {
-                if el.is_custom() && el.tag.as_str() == "code-block" {
-                    let mut hasher = DefaultHasher::new();
-                    el.get_text().hash(&mut hasher);
-                    custom![el.tag, el_key(&hasher.finish()), el.children]
-                } else {
-                    Node::Element(el)
+            Node::Element(mut el) => {
+                if let Some(AtValue::Some(el_key_value)) =
+                    el.attrs.vals.get(&At::from("data-el-key"))
+                {
+                    el.key = Some(el_key(el_key_value));
                 }
+                Node::Element(el)
             },
             _ => node,
         })
